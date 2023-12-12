ea55a9cf2453dc773003276865f27bad260d51c2
yasermoamd <yasermoamd@hotmail.com>
2023-12-12 07:48:52 +0000

    header updated

--- components/Header.php
+++ components/Header.php
@@ -5,7 +5,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
      rel="stylesheet"> 
 </head>
- 
+<?php session_start(); ?>
 <nav class="navbar">
         <div class="brand-title">Cake Cake</div>
         <a href="#" class="toggle-button">
@@ -21,7 +21,11 @@
           <li><a href="/contact.php">Contact US</a></li> 
           <div class="register_cart">
               <a href="/cart.php" class="material-icons">shopping_cart</a>
+              <?php if (isset($_SESSION['loggedin']) != false && $_SESSION['loggedin']): ?>
+              <a href="/auth/logout.php" class="material-icons logout">logout</a>
+              <?php else: ?>
               <a href="/auth/login.php" class="material-icons">person</a>
+              <?php endif; ?>
           </div>
         </ul>
         </div>
