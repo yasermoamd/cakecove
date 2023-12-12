652ade38469f60c16b6f43369477070abfa75d7a
yasermoamd <yasermoamd@hotmail.com>
2023-12-12 07:49:34 +0000

    dashboard init

---
+++ dashboard/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+     rel="stylesheet"> 
+    <link rel="stylesheet" href="./css/styles.css">
+    <title>CakeCove | Dashboard</title>
+</head>
+<body>
+    <main class="container">
+        <aside>
+            <h1>CakeCove - Dashboard</h1>
+            <div class="">
+                <p class="material-icons"></p>
+            </div>
+        </aside>
+        <section class="dash__container">
+            <section class="dash__info__container">
+                <div class="dash__title"><h1>Dashboard</h1></div>
+                <div class="content">
+                    <div class="box__1">
+                        <div class="box__details">
+                            <p class="material-icons">face</p>
+                        </div>
+                    </div>
+                    <div class="box__2"></div>
+                    <div class="box__3"></div>
+                    <div class="box__4"></div>
+                </div>
+            </section>
+            <h1>Product Table</h1>
+        </section>
+    </main>
+</body>
+</html>
